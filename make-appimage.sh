c83147b02951f26a30780ef53cb887470701774e
Link4Electronics <link4electronics@gmail.com>
2026-05-20 22:11:44 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q krita | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/256x256/apps/krita.png
+export DESKTOP=/usr/share/applications/org.kde.krita.desktop
+export STARTUPWMCLASS=
+export DEPLOY_QT=1
+export QT_DIR=qt6
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/krita* /usr/lib/kritaplugins /usr/lib/libkrita*.so* /usr/share/color-schemes /usr/share/color/icc/krita 
 
 # Additional changes can be done in between here
 
